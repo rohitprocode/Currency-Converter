15fe7bb9901390aa93b5322eb3b340ce2d68ff53
rohitprocode <rohit990797@gmail.com>
2023-08-26 23:45:46 +0530

    Button and TableBody

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log("Welcome to my Currency Converter");
 
 //API URL
-
 url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_jkSlYVAKYJnT2NTwkFvqXbK8mVlWGBnRc9lWLAyb';
+
+let button =  document.querySelector('.btn');
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log("Button is Clicked");
+})
+
+const tableBody = document.querySelector('tbody');
+tableBody.innerHTML = `
+<tr>
+<td>Data1</td>
+<td>Data2</td>
+<td>Data2</td>
+</tr>
+`
