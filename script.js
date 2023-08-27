acaea716979dfc45f939691bce781378f177a9e4
rohitprocode <rohit990797@gmail.com>
2023-08-27 19:57:25 +0530

    Write Narration in code

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ console.log("Welcome to my Currency Converter");
 //API URL
 url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_jkSlYVAKYJnT2NTwkFvqXbK8mVlWGBnRc9lWLAyb';
 
+// Submit Button Event Listener
 let button =  document.querySelector('.btn');
 button.addEventListener('click',(e)=>{
     e.preventDefault();
     console.log("Button is Clicked");
 })
 
+//Display Output in TableBody
 const tableBody = document.querySelector('tbody');
 tableBody.innerHTML = `
 <tr>
