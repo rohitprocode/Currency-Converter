60e0c1b063eb86a3072f34f20525524901cd9354
rohitprocode <rohit990797@gmail.com>
2023-08-27 20:16:06 +0530

    Script

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 console.log("Welcome to my Currency Converter");
 
 //API URL
-url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_jkSlYVAKYJnT2NTwkFvqXbK8mVlWGBnRc9lWLAyb';
+function populate(value, Currency) {
+    url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_jkSlYVAKYJnT2NTwkFvqXbK8mVlWGBnRc9lWLAyb&base_currency=';
 
-// Submit Button Event Listener
-let button =  document.querySelector('.btn');
-button.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log("Button is Clicked");
-})
-
-//Display Output in TableBody
-const tableBody = document.querySelector('tbody');
-tableBody.innerHTML = `
+    //Display Output in TableBody
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = `
 <tr>
 <td>Data1</td>
 <td>Data2</td>
 <td>Data2</td>
 </tr>
 `
+}
+// Submit Button Event Listener
+let button = document.querySelector('.btn');
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Button is Clicked");
+
+    const value = parseInt(document.getElementById('#InputNumber').value);
+    const Currency = document.getElementById('selectCurrency').value;
+    //Callback function
+    populate(value, Currency);
+})
+
